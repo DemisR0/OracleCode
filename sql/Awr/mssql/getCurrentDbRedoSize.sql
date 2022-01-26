56bcae166b064b6e29218f3c950f7a6361ccba20
DemisR0 <imhotep5000@hotmail.com>
2022-01-26 17:14:30 +0100

    final after tests

--- sql/Awr/mssql/getCurrentDbRedoSize.sql
+++ sql/Awr/mssql/getCurrentDbRedoSize.sql
@@ -1,27 +1,27 @@
-col begin_interval_time format a30
-set lines 160 pages 1000
-col end_interval_time format a30
-set colsep '|'
-alter session set nls_date_format='DD-MON-YYYY';
-with redo_sz as (
-SELECT  sysst.snap_id, sysst.instance_number, begin_interval_time ,end_interval_time ,  startup_time,
-VALUE - lag (VALUE) OVER ( PARTITION BY  startup_time, sysst.instance_number
-                ORDER BY begin_interval_time, startup_time, sysst.instance_number) stat_value,
-EXTRACT (DAY    FROM (end_interval_time-begin_interval_time))*24*60*60+
-            EXTRACT (HOUR   FROM (end_interval_time-begin_interval_time))*60*60+
-            EXTRACT (MINUTE FROM (end_interval_time-begin_interval_time))*60+
-            EXTRACT (SECOND FROM (end_interval_time-begin_interval_time)) DELTA
-  FROM sys.wrh$_sysstat sysst , DBA_HIST_SNAPSHOT snaps
-WHERE (sysst.dbid, sysst.stat_id) IN ( SELECT dbid, stat_id FROM sys.wrh$_stat_name WHERE  stat_name='redo size' )
+WITH raw_data AS (
+SELECT  sysst.dbid, sysst.snap_id, sysst.instance_number, begin_interval_time ,end_interval_time ,  startup_time,
+DATENAME(w,begin_interval_time) as wday, DATENAME(hh,begin_interval_time) as dhour,
+sysst.VALUE - lag (sysst.VALUE) OVER ( PARTITION BY  sysst.dbid, sysst.instance_number, startup_time
+                ORDER BY sysst.dbid, sysst.instance_number, startup_time, begin_interval_time) as stat_value,
+DATEDIFF(SECOND,BEGIN_INTERVAL_TIME,END_INTERVAL_TIME) as delta_t
+  FROM [AWRCPY].[WRH$_SYSSTAT] sysst , [AWRCPY].[WRM$_SNAPSHOT] snaps, [AWRCPY].[WRH$_STAT_NAME] stt_name
+WHERE stt_name.stat_name='redo size'
+AND stt_name.dbid=snaps.DBID
+AND stt_name.stat_id=sysst.STAT_ID
 AND snaps.snap_id = sysst.snap_id
 AND snaps.dbid =sysst.dbid
+AND snaps.error_count < 1
 AND sysst.instance_number=snaps.instance_number
-and begin_interval_time > sysdate-90
 )
-select instance_number,
-  to_date(to_char(begin_interval_time,'DD-MON-YYYY'),'DD-MON-YYYY') dt
-, sum(stat_value) redo1
-from redo_sz
-group by  instance_number,
-  to_date(to_char(begin_interval_time,'DD-MON-YYYY'),'DD-MON-YYYY')
-order by instance_number, 2;
+SELECT rd.wday as day , cast(rd.dhour as int) as hour ,round(SUM(rd.stat_value/rd.delta_t),0) as redo_bytes_sec
+FROM [AWRCPY].[WRM$_DATABASE_INSTANCE] dbi
+JOIN raw_data rd ON dbi.dbid=rd.dbid
+AND  dbi.instance_number=rd.instance_number
+AND dbi.STARTUP_TIME=rd.STARTUP_TIME
+WHERE dbi.INSTANCE_NAME in ('ANT1','ANT2','BFC1','BFC2','BIA1','BIA2','COD1','COD2','EAI1','EAI2','HOP1','HOP2','HR91','HR92','HRA1','HRA2','HYP1','HYP2','KFI1','KFI2')
+GROUP BY wday,dhour
+ORDER BY 1,2
+
+-- ('ANT1','ANT2','BFC1','BFC2','BIA1,'BIA2','COD1','COD2','EAI1','EAI2','HOP1','HOP2','HR91','HR92','HRA1','HRA2','HYP1,'HYP2','KFI1','KFI2')
+-- ('DSN1','DSN2','DWH1','DWH2','ETL1','ETL2','SAG1','SAG2','SGE1','SGE2')
+-- ('FAC1','FAC2')
