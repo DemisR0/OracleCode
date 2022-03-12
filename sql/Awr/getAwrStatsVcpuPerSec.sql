8b82cd7b916719070f2c516552b862b84f3f6c60
DemisR0 <imhotep5000@hotmail.com>
2022-03-12 17:19:57 +0100

    added query to avoid confusion du to shared dbid and instance name between old instances and moved instances

--- sql/Awr/getAwrStatsVcpuPerSec.sql
+++ sql/Awr/getAwrStatsVcpuPerSec.sql
@@ -23,3 +23,30 @@ from db_time_w_elapse)
 select instance_name,to_char(snp_date,'YYYYMMDD-HH24MI') snap_date, cpu_per_sec
 from cpu_per_sec_all_db
 order by instance_name,snap_date asc;
+
+-- Uniquement avec dbid instance_name startup_time pour eviter tout melange au niveau des instances qui partageraient le meme dbid et instance_name
+--
+col begin_interval_time format a30
+set lines 160 pages 1000
+col end_interval_time format a30
+col begin_interval_time for a25
+col end_interval_time for a25
+col startup_time for a25
+set colsep '|'
+alter session set nls_date_format='DD-MON-YYYY';
+SELECT  sysst.snap_id,sysst.stat_id, sysst.dbid,snaps.startup_time,sysst.instance_number, begin_interval_time ,end_interval_time,
+VALUE - lag (VALUE) OVER ( PARTITION BY  sysst.dbid, sysst.instance_number,snaps.startup_time,sysst.stat_id ORDER BY sysst.snap_id) stat_value,
+EXTRACT (DAY    FROM (end_interval_time-begin_interval_time))*24*60*60+
+            EXTRACT (HOUR   FROM (end_interval_time-begin_interval_time))*60*60+
+            EXTRACT (MINUTE FROM (end_interval_time-begin_interval_time))*60+
+            EXTRACT (SECOND FROM (end_interval_time-begin_interval_time)) DELTA
+FROM SYS.WRH$_SYS_TIME_MODEL sysst
+INNER JOIN SYS.WRH$_STAT_NAME n ON
+n.stat_id=sysst.stat_id
+n.dbid=sysst.dbid
+AND n.stat_name='DB CPU'
+INNER JOIN SYS.WRM$_SNAPSHOT snaps
+ON snaps.snap_id = sysst.snap_id
+AND snaps.dbid = sysst.dbid
+AND sysst.instance_number = snaps.instance_number
+and begin_interval_time > sysdate-10;
