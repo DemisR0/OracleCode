cf88ca9920ff8a740374bbc76c0a9286b170ba27
DemisR0 <imhotep5000@hotmail.com>
2022-04-05 22:54:36 +0200

    added check over pwd functions

--- python/autoBash/dbStats.py
+++ python/autoBash/dbStats.py
@@ -16,7 +16,7 @@ socks5 = '129.39.133.102'
 wallixKOP = '15.1.92.188'
 wallixKAz = '15.1.92.247'
 serverName = wallixKAz
-oracleServer = 'vml0dtbora000'
+oracleServer = 'vml0dtbora030'
 customer = 'korian'
 sudoUser = 'oracle'
 
@@ -27,21 +27,26 @@ scriptName=scriptCpu
 
 # Other variables
 sessionBName = customer + '_ssh'
-
-sessionName = tmux4sql.getTmuxSession(sessionBName)
+# changed the ~/.tmux.conf set-option -g history-limit 500000
+os.system('tmux set-option history-limit 500000')
 
 tUserPass = tmux4sql.readPwd(customer,'ssh',serverName)
+
+if tmux4sql.getXterm(sessionName) == False:
+    # if no xterm session has been created one must be created manually
+    sessionName = tmux4sql.getTmuxSession(sessionBName)
+
 tmux4sql.connectSsh(sessionName,serverName,tUserPass[0],tUserPass[1],socks5)
 tmux4sql.tmuxClear(sessionName)
 
 wUserPass = tmux4sql.readPwd(customer,'wallix',oracleServer)
-if not tmux4sql.wallixSelection(sessionName,oracleServer,wUserPass[0],wUserPass[1]):
+if not tmux4sql.wallixSelection(sessionName,oracleServer,wUserPass[0],wUserPass[1]) == False:
     logging.error('wallixSelection: Unable to select a server')
 
 if tmux4sql.sudo(sessionName,sudoUser) != True :
     logging.error('sudo : Unable to sudo as ' + sudoUser)
 
-os.system('tmux set-option history-limit 500000')
 #getMultiOraData(sessionName,BashScriptPath + scriptName)
 tmux4sql.getMultiOraData(sessionName,BashScriptPath + scriptName)
-tmux4sql.parseOutput(sessionName,'/mnt/c/temp' + sessionName + '.csv','|')
+tmux4sql.parseOutput(sessionName,'/mnt/c/temp/' + sessionName + '.csv','|')
+tmux4sql.exitSsh(sessionName,True,True)
